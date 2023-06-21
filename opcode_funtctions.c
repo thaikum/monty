de4953fa172653172203b2b534d479043581a78a
thaikum <fredthaiku@gmail.com>
2023-06-21 13:06:42 -0700

    pop fix

--- opcode_funtctions.c
+++ opcode_funtctions.c
@@ -70,7 +70,7 @@ void pop(stack_t **top, unsigned int line_number __attribute__((unused)))
 	}
 	else
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		free_global_holder();
 		exit(EXIT_FAILURE);
 	}
